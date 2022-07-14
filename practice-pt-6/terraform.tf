397cf060f06184040a314065b16d39cb65e4f234
PeriwinkleFTW <29137960+periwinkleFTW@users.noreply.github.com>
2022-07-13 23:11:31 -0600

    s3 backend settings

--- practice-pt-6/terraform.tf
+++ practice-pt-6/terraform.tf
@@ -1,12 +1,33 @@
 # terraform.tf
 
+# Local backend
 terraform {
-  backend "remote" {
-    hostname     = "app.terraform.io"
-    organization = "terraform-blog"
-
-    workspaces {
-      name = "demo-app-on-aws"
+  # Provider-specific settings
+  required_providers {
+    aws = {
+      version = ">= 2.7.0"
+      source  = "hashicorp/aws"
     }
   }
+  # Terraform version
+  required_version = ">= 0.14.9"
 }
+
+# terraform {
+#   backend "s3" {
+#     bucket         = "terraform-state-migration"
+#     key            = "dev/aws_infrastructure"
+#     region         = "ca-central-1"
+#     dynamodb_table = "tf-state-mig"
+#     encrypt        = true
+#   }
+#   # Provider-specific settings
+#   required_providers {
+#     aws = {
+#       version = ">= 2.7.0"
+#       source  = "hashicorp/aws"
+#     }
+#   }
+#   # Terraform version
+#   required_version = ">= 0.14.9"
+# }
